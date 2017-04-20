5834eb0f342040c7add29565b5763ffdd1dfe755
Oleh Ilnytskyi <oleh.ilnytskyi@cision.com>
2017-04-20 07:15:21 -0700

    Added clean script

---
+++ clean.ps1
@@ -0,0 +1,34 @@
+﻿# Deletes crap temp files, created by ChromeDriver
+$sTemp = "scoped_dir" # file mask
+$sTimeout = 1         # minutes to allow temp files to live before delete
+$sEverySeconds = 60   # run every
+
+
+function Get-TimeStamp {
+    $timeStamp = "[" + (Get-Date).ToShortDateString() + " " + ((Get-Date).ToShortTimeString()) + "]"
+    Return $timeStamp
+}
+
+for(;;) {
+ try {
+    Write-Host "$(Get-TimeStamp) Checking temp files with name $sTemp older then $sTimeout minutes"
+    Get-ChildItem $env:temp/ |
+    # file name template
+    Where {$_.name -Match $sTemp} |
+    # older then $sTimeout
+    Where-Object {$_.CreationTime -lt (date).addminutes(-$sTimeout)} | 
+    # Delete
+    # Remove-Item -Force -Recurse 
+    % { 
+      ForEach-Object { 
+        Write-Host "Deleting: $_" 
+        Remove-Item -Force -Recurse $_.fullname
+      }       
+    }
+ } catch [Exception] {
+    Write-Host "$(Get-TimeStamp) Some exception on deleting files"
+ }
+ # wait for a minute
+ Write-Host "$(Get-TimeStamp) Sleeping for $sEverySeconds seconds"
+ Start-Sleep $sEverySeconds
+}
